6580be845d6900117006d2c608181ea277d77a13
rcdtx <rcdehghan@gmail.com>
2021-12-14 00:29:58 -0600

    initial commit for nextjs project

---
+++ README.md
@@ -0,0 +1,3 @@
+## Spotify Top 50 Tracks by Genre
+
+This is a NextJS application that fetches the top 50 tracks by genre from Spotify's api.
---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <table>
+          {this.props.tracks.map(item => (
+            <tr>{item}</tr>
+          ))}
+        </table>
+      </div>
+    )
+  }
+}
+
+export async function getServerSideProps() {
+  const client_id = process.env.CLIENT_ID
+  const client_secret = process.env.CLIENT_SECRET
+  const base64Credentials = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+
+  const _getToken = async () => {
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + base64Credentials
+      },
+      body: 'grant_type=client_credentials'
+    });
+
+    const data = await result.json();
+    return data.access_token;
+  };
+
+  const _getTopTracks = async (token, genreId) => {
+    const result = await fetch(`https://api.spotify.com/v1/search?q=genre:%20${genreId}&type=track&limit=50`, {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + token }
+    });
+
+    const data = await result.json();
+    return data.tracks;
+  };
+
+  const token = await _getToken();
+  const tracks = await _getTopTracks(token, "EDM");
+
+  const topTracks = tracks.items;
+  const topTracksSorted = topTracks.sort((a, b) => b.popularity - a.popularity || Date.parse(b.album.release_date) - Date.parse(a.album.release_date));
+
+  const filteredTracks = topTracksSorted.map(item => item.name)
+
+  return {
+    props: {
+      tracks: filteredTracks
+    },
+  };
+}
