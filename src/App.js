da27e121c1fb3a81900fce7b862cdabf57f4edb9
rcdhtx <rcdhtx@gmail.com>
2022-05-30 15:17:40 -0400

    updated to use material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import Button from '@material-ui/core/Button'
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 import './App.css';
+import Column from './components/Column';
+
+
+const genreList = {
+  'Alternative Rock': '0aXgSLLFYRmT87Fit4lU35',
+  'Ambient': '1pbOkTqfvFgMMzKfCnTOgG',
+  'Blues': '5HUYim9Ao38y3lzJuuJy6a',
+  'Classical': '0yYCyUiJDnlU01jjLXxErd',
+  'Country': '4zZ87MUbSo1YyBj50AKQWv',
+  'Dancehall': '0d2lK8zr1wzwCudVNDvccw',
+  'Deep House': '7vD3EPo8uGyqJqqckZ3e6M',
+  'Disco': '70ZCTyQaVN8iwsuCuYX1F6',
+  'Drum and Bass': '5ALTYHhJzCasMIZuo0MqOq',
+  'Dubstep': '6TgD960XDPCLsBvZJIjq4i',
+  'EDM': '51V1kDRorMxXDHxmaImWCE',
+  'Electronic': '7qHoNdW8d4DCjnbDZVoOMn',
+  'Folk': '49B54TpeFQLFDpgl9eE8ry',
+  'Rap': '3mdw5yxk21OIDtq6nZS9jC',
+  'House': '5VC6LnsZoZEtUNjCQqEnKm',
+  'Indie': '5M3eIQcuAIWKbIwhoCnwew',
+  'Jazz': '7JEOVv3bn1sUnSuWEhVHVR',
+  'Latin': '6rdVDi8QGSVeidyVDeClPj',
+  'Metal': '3VxZpLk7y8GK65jJmep1ux',
+  'Pop': '3mQLQ5ZVpjixLv4P6h1RMn',
+  'Rhythm and Blues': '0CE9O4AdppRNfiQIsPAXkk',
+  'Reggae': '2iQc86tECGhSjmSx1zViIC',
+  'Reggaeton': '0SVoqDG5ReplwbaqROD1QS',
+  'Rock': '3GhEQkyQCTZOzzCOw8cNP5',
+  'Soundtrack': '5uKhl4UAg4EH8QxaToxDT7',
+  'Techno': '6kio0EUKcGw4dcc2h9XSH5',
+  'Trance': '4OB0bbEGXiKnXxZAo0eGUN',
+  'Trap': '6mxg4NxqkR8cr1o8hBPG5A'
+}
+
+console.log(Object.keys(genreList))
 
 export default function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(Array(50).fill(1).map((x, y) => x + y));
 
   useEffect(() => {
     fetch("/api/get_playlists")
@@ -12,17 +50,28 @@ export default function App() {
   }, []);
 
   return (
-    <div>
+    <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
       <img src={logo} className="App-logo" alt="logo" />
-      <h3>Enter Genre:</h3>
-      <input></input><button>search</button>
-      
-      <h3>EDM</h3>
-      <ol>
-        {data.map((el, index) => (
-          <li key={index}>{el}</li>
+
+      <h3>Add Genre:</h3>
+      <TextField id="outlined-basic" label="genre" variant="outlined" />
+      <Button variant="contained">Add</Button>
+
+      <Grid container direction="row" spacing={4}>
+        {Array.from(Object.keys(genreList)).map((genre, index) => (
+          <Grid item key={index}>
+          <Column genre={genre} data={data}></Column>
+          </Grid>
         ))}
-      </ol>
-    </div>
+      </Grid>
+    </>
   );
 }
---
+++ src/components/Column.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+export default function Column({ genre, data }) {
+    return (
+        <>
+            <TableContainer sx={{ maxHeight: 600 }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                {genre}
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((el, index) => (
+                            <TableRow key={index}>
+                                <TableCell key={index} align={'center'}>
+                                    {index + 1}. {el}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
